816f6e80f55b163cdb2802bdce184851e7003db2
Mugur Rus <mugur.rus@gmail.com>
2018-09-20 15:49:40 +0300

    make mng scripts compatible with python 3.5 (#42)

--- fixcollectionsnames.py
+++ fixcollectionsnames.py
@@ -8,7 +8,7 @@ from src.common import get_collections_data
 if __name__ == '__main__':
     collections, _, _, _ = get_collections_data()
     for collection in collections:
-        call = ['docker-compose', 'run', '--rm', f'snoop--{collection}', './manage.py',
+        call = ['docker-compose', 'run', '--rm', 'snoop--' + collection, './manage.py',
                 'updatename', collection.lower(), collection]
         print(call, end='', flush=True)
         process = subprocess.run(call, stdout=PIPE, stderr=PIPE)
