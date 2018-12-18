bab8c99f26be6258e950d607836c23004982b41d
Mugur Rus <mugur.rus@gmail.com>
2018-12-18 22:11:32 +0200

    update urls for doc view

--- templates/collection-steps.sh
+++ templates/collection-steps.sh
@@ -5,6 +5,8 @@ echo "Starting with added containers using docker-compose..."
 docker-compose up -d
 echo "Waiting for PostgreSQL to start..."
 docker-compose run --rm snoop--{{ collection_name }} /wait
+echo "Updating search models..."
+docker-compose run --rm search ./manage.py migrate
 echo "Creating the data model..."
 docker-compose run --rm snoop--{{ collection_name }} ./manage.py migrate
 echo "Dispatching tasks in Snoop..."
