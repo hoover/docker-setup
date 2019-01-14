5bfc98df89ed4477e850e2fd9545ecb8adf2236a
Mugur Rus <mugur.rus@gmail.com>
2019-01-14 18:04:41 +0200

    fix: add index creation step on new collection (#59)
    * fix: add index creation step on new collection
    
    * replace snoop multiple steps with 'initcollection'

--- templates/collection-steps.sh
+++ templates/collection-steps.sh
@@ -7,10 +7,8 @@ echo "Waiting for PostgreSQL to start..."
 docker-compose run --rm snoop--{{ collection_name }} /wait
 echo "Updating search models..."
 docker-compose run --rm search ./manage.py migrate
-echo "Creating the data model..."
-docker-compose run --rm snoop--{{ collection_name }} ./manage.py migrate
-echo "Dispatching tasks in Snoop..."
-docker-compose run --rm snoop--{{ collection_name }} ./manage.py rundispatcher
+echo "Initializing the collection database, index, running dispatcher..."
+docker-compose run --rm snoop--{{ collection_name }} ./manage.py initcollection
 echo "Adding the collection to search..."
 docker-compose run --rm search ./manage.py addcollection {{ collection_name }} --index {{ collection_index }} http://snoop--{{ collection_name }}/json
 echo "Done."
